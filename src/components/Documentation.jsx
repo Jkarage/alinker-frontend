71c3102f1d76ccc024da373c88d765f758f95ebf
Jkarage <josephbkarage@gmail.com>
2023-01-22 10:44:21 +0300

    Docs page initial commit

---
+++ src/components/Documentation.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+const Documentation = () => {
+  return (
+    <div>
+      <blockquote className=" text-center">
+        The tool is built with Golang in the Back-End and React in the frontend.
+        This is for showcasing my skills in the progress of finding a remote
+        Software Engineer Job. It&apos;s an open-source project the links can be
+        found here: <Link href="https://alinker.tk/aGWyYK4Uw"></Link>
+      </blockquote>
+    </div>
+  );
+};
+
+export default Documentation;

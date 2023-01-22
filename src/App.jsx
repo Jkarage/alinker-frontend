f7473c6d96d2e73f3d4ea6eed3f55973ad636fd1
Jkarage <josephbkarage@gmail.com>
2023-01-22 10:11:27 +0300

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Routes, Route } from "react-router-dom";
+import NavigationBar from "./components/NavigationBar";
+import Pricing from "./components/Pricing";
+import Registration from "./components/Registration";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Documentation from "./components/Doocumentation";
+import ResetPassword from "./components/ResetPassword";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      scaleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   return (
     <div>
-      <h2>Hello World, Alinker Initial commits</h2>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <NavigationBar />
+          <Routes>
+            <Route path="/register" element={<Registration />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/reset" element={<ResetPassword />} />
+            <Route path="/pricing" element={<Pricing />} />
+            <Route path="/docs" element={<Documentation />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </QueryClientProvider>
+      </BrowserRouter>
     </div>
   );
 };

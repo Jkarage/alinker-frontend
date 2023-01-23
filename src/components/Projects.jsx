dc160cc25d4ca47c08346eceb6dc9f52ee0912ff
Jkarage <josephbkarage@gmail.com>
2023-01-23 12:10:30 +0300

    Updates projects component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -36,19 +36,7 @@ const Projects = () => {
             to="/"
             className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
           >
-            <svg
-              className="w-4 h-4 mr-2"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z"
-                clipRule="evenodd"
-              ></path>
-            </svg>{" "}
-            Download ZIP
+            Github Repository
           </Link>
         </li>
         <li className="mb-10 ml-6">
@@ -73,9 +61,15 @@ const Projects = () => {
           <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
             Released on December 7th, 2022
           </time>
-          <p className="text-base font-normal text-gray-500 dark:text-gray-500">
+          <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-500">
             Written in Golang
           </p>
+          <Link
+            to="/"
+            className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+          >
+            Github Repository
+          </Link>
         </li>
         <li className="ml-6">
           <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-200 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
@@ -99,9 +93,15 @@ const Projects = () => {
           <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
             Released on December 2nd, 2022
           </time>
-          <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-            The Back-End Api for this app.
+          <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+            The Back-End Api for this Web application.
           </p>
+          <Link
+            to="/"
+            className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+          >
+            Github Repository
+          </Link>
         </li>
       </ol>
     </div>
